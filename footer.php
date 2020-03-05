c9de57119648987c34ffca28807cd3df745773e1
Yasmine Aikio <yasmineaikio@hotmail.com>
2020-03-05 10:08:35 +0100

    sass-structure setup

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
   </div>
 <footer>
-  <h2>Det här är en footer</h2>
+  <div class="footer1">
+    <h2>Det här är en footer</h2>
+    seg
+  </div>
+  <div class="footer2">
+    <h2>Det här är en footer</h2>
+    seg
+  </div>
 </footer>
-
 		<?php wp_footer(); ?>
-
 	</body>
 </html>
