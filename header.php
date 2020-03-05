30b2fc2974c6dfc256d030b2444910235bd0b2b5
Yasmine Aikio <yasmineaikio@hotmail.com>
2020-03-05 11:43:52 +0100

    fullscreen menu on click

--- header.php
+++ header.php
@@ -8,6 +8,51 @@
 	</head>
 	<body <?php body_class(); ?> >
     <header>
-      <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+			<div class="menu-icon">
+				<span class="menu-icon__line menu-icon__line-left"></span>
+				<span class="menu-icon__line"></span>
+				<span class="menu-icon__line menu-icon__line-right"></span>
+			</div>
+
+			<div class="nav">
+					<?php
+					 	wp_nav_menu( array( 'theme_location' => 'header-menu',
+						'container_class' => 'nav__content',
+						'menu_class' => 'nav__list')
+					); ?>
+			</div>
+
     </header>
 		<div class="main">
+
+<script>
+console.clear();
+
+const app = (() => {
+	let body;
+	let menu;
+	let menuItems;
+
+	const init = () => {
+		body = document.querySelector('body');
+		menu = document.querySelector('.menu-icon');
+		menuItems = document.querySelectorAll('.nav__list-item');
+
+		applyListeners();
+	}
+
+	const applyListeners = () => {
+		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+	}
+
+	const toggleClass = (element, stringClass) => {
+		if(element.classList.contains(stringClass))
+			element.classList.remove(stringClass);
+		else
+			element.classList.add(stringClass);
+	}
+
+	init();
+})();
+
+</script>
